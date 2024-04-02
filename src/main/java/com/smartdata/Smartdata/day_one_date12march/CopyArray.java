56408cb910a63cbf3c0e58d3e7abb9300842a54f
ankitapajai <ankitapajai@gmail.com>
2024-04-02 08:19:22 +0530

    Copy array

--- src/main/java/com/smartdata/Smartdata/day_one_date12march/CopyArray.java
+++ src/main/java/com/smartdata/Smartdata/day_one_date12march/CopyArray.java
@@ -1,2 +1,24 @@
-package com.smartdata.Smartdata.day_one_date12march;public class CopyArray {
+package com.smartdata.Smartdata.day_one_date12march;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = new int[arr1.length];
+
+        for(int i=0; i<arr1.length; i++){
+            arr2[i] = arr1[i];
+        }
+        System.out.println("First array elements: ");
+        for(int i=0; i<arr1.length; i++){
+            System.out.print(" " +arr1[i]);
+        }
+
+        System.out.println(" ");
+
+        System.out.println("second array elements: ");
+        for(int i=0; i<arr2.length; i++){
+            System.out.print(" " +arr2[i]);
+        }
+
+    }
 }
