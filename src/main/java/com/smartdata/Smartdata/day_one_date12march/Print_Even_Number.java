11c36d5c53a0f4bf9a4ef07c20adfa0653a99127
ankitapajai <ankitapajai@gmail.com>
2024-04-02 08:19:59 +0530

    print even number

--- src/main/java/com/smartdata/Smartdata/day_one_date12march/Print_Even_Number.java
+++ src/main/java/com/smartdata/Smartdata/day_one_date12march/Print_Even_Number.java
@@ -1,2 +1,13 @@
-package com.smartdata.Smartdata.day_one_date12march;public class Print_Even_Number {
+package com.smartdata.Smartdata.day_one_date12march;
+
+public class Print_Even_Number {
+    public static void main(String[] args) {
+
+        // Print even numbers between 1 and 20 using a for loop.
+        for(int i = 0; i <= 20; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
 }
