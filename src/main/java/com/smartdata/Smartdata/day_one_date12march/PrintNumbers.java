c4d92a5717b420e7283af724f7c640d0215feb46
ankitapajai <ankitapajai@gmail.com>
2024-04-07 16:08:24 +0530

    Numbers program

--- src/main/java/com/smartdata/Smartdata/day_one_date12march/PrintNumbers.java
+++ src/main/java/com/smartdata/Smartdata/day_one_date12march/PrintNumbers.java
@@ -1,2 +1,12 @@
-package com.smartdata.Smartdata.day_one_date12march;public class PrintNumbers {
+package com.smartdata.Smartdata.day_one_date12march;
+
+public class PrintNumbers {
+    public static void main(String[] args) {
+
+        // write a program to print a numbers from 1 to 10 using a for loop...
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
 }
--- src/main/java/com/smartdata/Smartdata/day_one_date12march/Print_Odd_Number.java
+++ src/main/java/com/smartdata/Smartdata/day_one_date12march/Print_Odd_Number.java
@@ -1,2 +1,14 @@
-package com.smartdata.Smartdata.day_one_date12march;public class Print_Odd_Number {
+package com.smartdata.Smartdata.day_one_date12march;
+
+public class Print_Odd_Number {
+    public static void main(String[] args) {
+        // Print odd numbers between 1 and 20 using a while loop.
+        int number=1;
+        while(number <= 20){
+            if(number % 2 != 0){
+                System.out.println(number);
+            }
+            number++;
+        }
+    }
 }
