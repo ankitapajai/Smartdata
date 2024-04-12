fd2fe751c0d3e6a69490246b1d2be8d8c729b1f4
ankitapajai <ankitapajai@gmail.com>
2024-04-12 17:27:54 +0530

    length of array

---
+++ src/main/java/com/smartdata/Smartdata/Arrays/Length_Of_Array.java
@@ -0,0 +1,8 @@
+package com.smartdata.Smartdata.Arrays;
+
+public class Length_Of_Array {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,3,4,5,6};
+        System.out.println("Length of Array is: " +arr.length);
+    }
+}
